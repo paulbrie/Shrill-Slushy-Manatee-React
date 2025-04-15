7cd546c3a12c6a5d59fc3f981d3813082b27bcd0
Paul BRIE <paul.brie@teleporthq.io>
2025-04-15 14:12:12 -0400

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -12,6 +12,11 @@ const Home = (props) => {
         <meta property="og:title" content="Shrill Slushy Manatee" />
       </Helmet>
       <span>Test Github</span>
+      <img
+        src="https://images.unsplash.com/photo-1742201876722-85a042294575?ixid=M3w5MTMyMXwwfDF8YWxsfDJ8fHx8fHx8fDE3NDQ3Mzk0MDF8&amp;ixlib=rb-4.0.3&amp;w=200"
+        alt="image"
+        className="home-image"
+      />
     </div>
   )
 }
