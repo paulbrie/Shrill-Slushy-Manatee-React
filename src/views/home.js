1824f6dde5bfcaa64f355c47cc2c102acd01010f
Paul BRIE <paul.brie@teleporthq.io>
2025-04-16 09:51:29 -0400

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -15,13 +15,17 @@ const Home = (props) => {
       <img
         alt="image"
         src="/nume%C3%AC%C2%81riser%2014.jpeg"
-        className="home-image1"
+        className="home-image10"
       />
-      <img alt="image" src="/ap%2040.jpeg" className="home-image2" />
-      <img alt="image" src="/scan%209.jpeg" className="home-image3" />
-      <img alt="image" src="/turzii%201.png" className="home-image4" />
-      <img alt="image" src="/turzii%202.png" className="home-image5" />
-      <img alt="image" src="/turzii%203.png" className="home-image6" />
+      <img alt="image" src="/ap%2040.jpeg" className="home-image11" />
+      <img alt="image" src="/scan%209.jpeg" className="home-image12" />
+      <img alt="image" src="/turzii%201.png" className="home-image13" />
+      <img alt="image" src="/turzii%202.png" className="home-image14" />
+      <img alt="image" src="/turzii%203.png" className="home-image15" />
+      <img src="/turzii4.png-200h.png" alt="image" className="home-image16" />
+      <img src="/turzii%205-200h.png" alt="image" className="home-image17" />
+      <img src="/turzii%20332-200h.png" alt="image" className="home-image18" />
+      <img src="/turzii%206-200h.png" alt="image" className="home-image19" />
     </div>
   )
 }
