95b069013f6fddb0d658d9fbab3fda99be6e4b9d
Paul BRIE <paul.brie@teleporthq.io>
2025-04-16 09:45:09 -0400

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -14,10 +14,14 @@ const Home = (props) => {
       <span>Test Github</span>
       <img
         alt="image"
-        src="/nume%C3%AC%C2%81riser%2014-200w.jpeg"
+        src="/nume%C3%AC%C2%81riser%2014.jpeg"
         className="home-image1"
       />
-      <img src="/ap%2040.jpeg" alt="image" className="home-image2" />
+      <img alt="image" src="/ap%2040.jpeg" className="home-image2" />
+      <img alt="image" src="/scan%209.jpeg" className="home-image3" />
+      <img alt="image" src="/turzii%201.png" className="home-image4" />
+      <img alt="image" src="/turzii%202.png" className="home-image5" />
+      <img alt="image" src="/turzii%203.png" className="home-image6" />
     </div>
   )
 }
